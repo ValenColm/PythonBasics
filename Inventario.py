32694fae2553a844db32f71393cbe3520dbe5d54
Valentina <dismarybolivar1711@gmail.com>
2025-11-04 19:04:06 -0500

    FirstCommit

---
+++ Inventario.py
@@ -0,0 +1,18 @@
+#Solicitar al usuario los datos del producto
+name = input("Ingresa el nombre del producto que compraste => ")
+price = input("Ingresa el precio del producto que compraste => ")
+quantity = input("Ingresa la cantidad del producto que compraste => ")
+
+#Verificar que el precio y la cantidad sean números válidos
+if price.isdigit() and quantity.isdigit():
+    #Convertir los valores a enteros para realizar operaciones matemáticas
+    price = int(price)
+    quantity = int(quantity)
+    #Calculo del costo total del producto.
+    costo_total = price * quantity
+    #Mostrar los datos del producto y resultado del calculo
+    print(f"Producto = {name} | Precio = {price} | Cantidad = {quantity} | Costo Total = {costo_total}")
+
+#Mostrar un mensaje de error, si los valores ingresados son invalidos.
+else:
+    print("Error: Ingresa solo números válidos para precio y cantidad.")
